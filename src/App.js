eeff7b380ba3a8a802beadc68cc0ff19c30386cd
Rozen Raymond <rozen.raymond@lendi.com.au>
2019-10-29 08:42:08 +1100

    remove unused variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
 import "./App.css";
