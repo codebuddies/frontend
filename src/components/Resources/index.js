31af0b06fdcc73817972e32b4dfecd85ffa9345a
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2019-11-04 20:37:43 -0800

    Remove unused referecen to Link

--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import PersonalMenu from "../PersonalMenu";
 import Search from "../Search";
