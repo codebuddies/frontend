a600ef03040668e4c9efec082a1460f184ef7699
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2019-12-19 21:12:51 -0800

    Create login and signup components

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+
+function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem("token") ? true : false
+  );
+
+  const handleLogin = (e, data) => {
+    console.log(e);
+    e.preventDefault();
+    fetch("http://localhost:3001/token-auth/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem("token", json.token);
+        setLoggedIn(true);
+        setUsername(json.user.username);
+      });
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h4>Log In</h4>
+      <label htmlFor="username">Username</label>
+      <TextField
+        id="username"
+        label="Username"
+        name="username"
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+      />
+      <TextField
+        id="password"
+        label="Password"
+        name="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Primary
+      </Button>
+    </form>
+  );
+}
+
+export default LoginForm;
