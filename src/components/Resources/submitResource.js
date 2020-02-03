56f028381b6329c365de7f631fdb5106feca0eb0
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2020-02-03 01:47:05 -0800

    WIP: implemeent react hook form

--- src/components/Resources/submitResource.js
+++ src/components/Resources/submitResource.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import CreatableSelect from 'react-select/creatable';
 import clsx from 'clsx';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import {
   Grid,
@@ -85,6 +86,8 @@ const SubmitResource = matchProps => {
   const classes = useStyles();
   const inputLabel = useRef(null);
   const [labelWidth, setLabelWidth] = useState(0);
+  const { register, handleSubmit, errors } = useForm();
+
   useEffect(() => {
     setLabelWidth(inputLabel.current.offsetWidth);
   }, []);
@@ -114,7 +117,7 @@ const SubmitResource = matchProps => {
     setTags(newValue);
   };
 
-  const handleSubmit = () => {
+  const onSubmit = () => {
     axios
       .post('http://localhost:3001/resources', values)
       .then(function(response) {
@@ -149,159 +152,174 @@ const SubmitResource = matchProps => {
             </Breadcrumbs>
           </Paper>
         </div>
-
-        <TextField
-          required
-          id="outlined-dense"
-          label="URL"
-          className={clsx(classes.textField, classes.dense)}
-          margin="dense"
-          variant="outlined"
-          fullWidth
-          value={values.url}
-          onChange={handleChange('url')}
-        />
-
-        <TextField
-          required
-          id="outlined-dense"
-          label="Title"
-          className={clsx(classes.textField, classes.dense)}
-          margin="dense"
-          variant="outlined"
-          fullWidth
-          value={values.title}
-          onChange={handleChange('title')}
-        />
-
-        <TextField
-          required
-          id="outlined-dense"
-          label="Description"
-          className={clsx(classes.textField, classes.dense)}
-          margin="dense"
-          variant="outlined"
-          fullWidth
-          value={values.description}
-          onChange={handleChange('description')}
-        />
-
-        <TextField
-          id="outlined-dense"
-          label="Year Updated"
-          className={clsx(classes.textField, classes.dense)}
-          margin="dense"
-          variant="outlined"
-          value={values.year}
-          onChange={handleChange('year')}
-        />
-        <FormGroup aria-label="position" row className={classes.freeSwitch}>
-          <FormControlLabel
-            checked={values.freeResource}
-            value="start"
-            control={<Switch color="primary" />}
-            label="Free"
-            labelPlacement="start"
-            onChange={handleSwitchChange('freeResource')}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            required
+            id="outlined-dense"
+            label="URL"
+            className={clsx(classes.textField, classes.dense)}
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            value={values.url}
+            onChange={handleChange('url')}
+            name="url"
+            ref={register({ required: true, maxLength: 400 })}
           />
-        </FormGroup>
+          {errors.url && 'URL is required'}
 
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel
-            ref={inputLabel}
-            htmlFor="outlined-media-type-native-simple"
-            className={classes.label}
-          >
-            Media Type
-          </InputLabel>
-          <Select
-            native
-            value={values.mediaType}
-            inputProps={{
-              name: 'media-type',
-              id: 'outlined-media-type-native-simple',
-            }}
-            className={classes.dense}
+          <TextField
+            required
+            id="outlined-dense"
+            label="Title"
+            className={clsx(classes.textField, classes.dense)}
             margin="dense"
-            labelWidth={labelWidth}
-            onChange={handleChange('mediaType')}
-          >
-            <option value="" />
-            {mediaTypes.map(type => (
-              <option key={type} value={type}>
-                {type}
-              </option>
-            ))}
-          </Select>
-        </FormControl>
+            variant="outlined"
+            fullWidth
+            name="title"
+            ref={register}
+            value={values.title}
+            onChange={handleChange('title')}
+          />
 
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel
-            ref={inputLabel}
-            htmlFor="outlined-level-native-simple"
-            className={classes.label}
-          >
-            Level
-          </InputLabel>
-          <Select
-            native
-            value={values.level}
-            inputProps={{
-              name: 'level',
-              id: 'outlined-level-native-simple',
-            }}
-            className={classes.dense}
+          <TextField
+            required
+            id="outlined-dense"
+            label="Description"
+            className={clsx(classes.textField, classes.dense)}
             margin="dense"
-            labelWidth={labelWidth}
-            onChange={handleChange('level')}
-          >
-            <option value="" />
-            <option value="beginner">Beginner</option>
-            <option value="intermediate">Intermediate</option>
-            <option value="advanced">Advanced</option>
-          </Select>
-        </FormControl>
+            variant="outlined"
+            fullWidth
+            name="description"
+            ref={register}
+            value={values.description}
+            onChange={handleChange('description')}
+          />
 
-        <TextField
-          id="outlined-dense"
-          label="Your Review"
-          value={values.review}
-          className={clsx(classes.textField, classes.dense)}
-          margin="dense"
-          variant="outlined"
-          fullWidth
-          multiline
-          rowsMax="4"
-          onChange={handleChange('review')}
-        />
+          <TextField
+            id="outlined-dense"
+            label="Year Updated"
+            className={clsx(classes.textField, classes.dense)}
+            margin="dense"
+            variant="outlined"
+            name="year_updated"
+            ref={register}
+            value={values.year}
+            onChange={handleChange('year')}
+          />
+          <FormGroup aria-label="position" row className={classes.freeSwitch}>
+            <FormControlLabel
+              checked={values.freeResource}
+              value="start"
+              control={<Switch color="primary" />}
+              label="Free"
+              name="price"
+              ref={register}
+              labelPlacement="start"
+              onChange={handleSwitchChange('freeResource')}
+            />
+          </FormGroup>
 
-        <CreatableSelect
-          className={classes.select}
-          isMulti
-          onChange={handleTagChange}
-          placeholder="Tags"
-          options={defaultTags}
-          styles={{
-            control: styles => ({
-              ...styles,
-              borderColor: 'hsl(0, 0%, 77%)',
-              ':hover': { borderColor: 'rgba(0, 0, 0, 0.87)' },
-              ':focus': { border: '2px solid #3f51b5' },
-            }),
-            placeholder: styles => ({ ...styles, color: '#757575' }),
-          }}
-          value={tags}
-        />
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel
+              ref={inputLabel}
+              htmlFor="outlined-media-type-native-simple"
+              className={classes.label}
+            >
+              Media Type
+            </InputLabel>
+            <Select
+              native
+              value={values.mediaType}
+              inputProps={{
+                name: 'media-type',
+                id: 'outlined-media-type-native-simple',
+              }}
+              name="media_type"
+              ref={register}
+              className={classes.dense}
+              margin="dense"
+              labelWidth={labelWidth}
+              onChange={handleChange('mediaType')}
+            >
+              <option value="" />
+              {mediaTypes.map(type => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
 
-        <FormGroup aria-label="position" className={classes.submit}>
-          <Button
-            variant="contained"
-            className={classes.button}
-            onClick={handleSubmit}
-          >
-            Submit
-          </Button>
-        </FormGroup>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel
+              ref={inputLabel}
+              htmlFor="outlined-level-native-simple"
+              className={classes.label}
+            >
+              Level
+            </InputLabel>
+            <Select
+              native
+              value={values.level}
+              inputProps={{
+                name: 'level',
+                id: 'outlined-level-native-simple',
+              }}
+              ref={register}
+              className={classes.dense}
+              margin="dense"
+              labelWidth={labelWidth}
+              onChange={handleChange('level')}
+            >
+              <option value="" />
+              <option value="beginner">Beginner</option>
+              <option value="intermediate">Intermediate</option>
+              <option value="advanced">Advanced</option>
+            </Select>
+          </FormControl>
+
+          <TextField
+            id="outlined-dense"
+            label="Your Review"
+            value={values.review}
+            className={clsx(classes.textField, classes.dense)}
+            margin="dense"
+            variant="outlined"
+            fullWidth
+            multiline
+            rowsMax="4"
+            onChange={handleChange('review')}
+            name="your_review"
+            ref={register}
+          />
+
+          <CreatableSelect
+            className={classes.select}
+            isMulti
+            onChange={handleTagChange}
+            placeholder="Tags"
+            options={defaultTags}
+            styles={{
+              control: (styles, state) => ({
+                ...styles,
+                borderColor: 'hsl(0, 0%, 77%)',
+                ':hover': { borderColor: 'rgba(0, 0, 0, 0.87)' },
+                ':focus': { border: '2px solid #3f51b5' },
+              }),
+              placeholder: styles => ({ ...styles, color: '#757575' }),
+            }}
+            value={tags}
+            name="tags"
+            ref={register}
+          />
+
+          <FormGroup aria-label="position" className={classes.submit}>
+            <Button variant="contained" className={classes.button}>
+              Submit
+            </Button>
+          </FormGroup>
+        </form>
       </Grid>
     </Grid>
   );
