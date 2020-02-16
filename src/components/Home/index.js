d1de9ff9f072ce97f007b88e6efebe57bef16c81
Angelo Cordon <angelocordon@gmail.com>
2020-02-16 12:52:13 -0800

    Update Homepage layout and copy

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Grid, Box } from '@material-ui/core/';
 
-function Home() {
-  return <h2>Home</h2>;
-}
+const Home = () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <Box
+          component="h1"
+          fontWeight="fontWeightBold"
+          fontSize={22}
+          color="primary.main"
+        >
+          CodeBuddies
+        </Box>
+        <Box component="h2" lineHeight={1.5} mb={4}>
+          We're a global community of people who help each other become better
+          at software development through conversations on Slack and
+          peer-to-peer organized study groups and virtual hangouts.
+        </Box>
+        <Box component="h3" fontSize={18} fontWeight={600}>
+          Join the conversations - Sign Up.
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Home;
