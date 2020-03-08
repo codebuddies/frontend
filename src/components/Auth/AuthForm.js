a4e21a6b8b788ca47fa342d79a35c54bf283de56
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2020-03-08 00:39:18 -0800

    Fix paths

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Box, Paper } from '@material-ui/core/';
-import SignUpForm from './components/SignUpForm';
-import LoginForm from './components/LoginForm';
+import SignUpForm from './SignUpForm';
+import LoginForm from './LoginForm';
 
 const AuthForm = () => {
   const [activeSignUpForm, setActiveSignUpform] = useState(true);
