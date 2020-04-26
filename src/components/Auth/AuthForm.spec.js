d842ed6ec80f6527aac188608318eca39c028654
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2020-04-26 00:31:10 -0700

    first attempt to fix signup test warning

--- src/components/Auth/AuthForm.spec.js
+++ src/components/Auth/AuthForm.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, act } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter } from 'react-router-dom';
 import AuthForm from './AuthForm';
@@ -40,46 +40,48 @@ describe('AuthForm', () => {
 });
 
 describe('Signup', () => {
-  it('Register a new user on the signup form', async () => {
-    const { getByText, getByLabelText } = render(
-      <BrowserRouter>
-        <SignUpForm />
-      </BrowserRouter>
-    );
+  fit('Register a new user on the signup form', async () => {
+    await act(async () => {
+      const { getByText, getByLabelText } = render(
+        <BrowserRouter>
+          <SignUpForm />
+        </BrowserRouter>
+      );
 
-    const mockRegisterResponse = jest.fn().mockResolvedValue({
-      data: {
-        username: 'Carolyne.Carter',
-        token:
-          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IkNhcm9seW5lLkNhcnRlciIsImlhdCI6MTU4NDMzODQ4NiwiZXhwIjoxNTg0MzQyMDg2LCJ1c2VyX2lkIjo4MCwib3JpZ19pYXQiOjE1ODQzMzg0ODZ9.saO6OCOKV1uwHjTbM-iDGmhbkMNCnzrGFj4TBYnTv2E',
-        first_name: 'Carolyne',
-        last_name: 'Carter',
-        email: 'Carolyne.Carter@yahoo.com',
-      },
-    });
+      const mockRegisterResponse = jest.fn().mockResolvedValue({
+        data: {
+          username: 'Carolyne.Carter',
+          token:
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IkNhcm9seW5lLkNhcnRlciIsImlhdCI6MTU4NDMzODQ4NiwiZXhwIjoxNTg0MzQyMDg2LCJ1c2VyX2lkIjo4MCwib3JpZ19pYXQiOjE1ODQzMzg0ODZ9.saO6OCOKV1uwHjTbM-iDGmhbkMNCnzrGFj4TBYnTv2E',
+          first_name: 'Carolyne',
+          last_name: 'Carter',
+          email: 'Carolyne.Carter@yahoo.com',
+        },
+      });
 
-    fireEvent.change(getByLabelText(/username/i), {
-      target: { value: 'Carolyne.Carter' },
-    });
+      fireEvent.change(getByLabelText(/username/i), {
+        target: { value: 'Carolyne.Carter' },
+      });
 
-    fireEvent.change(getByLabelText(/password/i), {
-      target: { value: 'password' },
-    });
-    fireEvent.change(getByLabelText(/email/i), {
-      target: { value: 'Carolyne.Carter@yahoo.com' },
-    });
-    fireEvent.change(getByLabelText(/first name/i), {
-      target: { value: 'Carolyne' },
-    });
-    fireEvent.change(getByLabelText(/last name/i), {
-      target: { value: 'Carter' },
-    });
-    const submit = getByText('Sign Up');
-    fireEvent.click(submit);
+      fireEvent.change(getByLabelText(/password/i), {
+        target: { value: 'password' },
+      });
+      fireEvent.change(getByLabelText(/email/i), {
+        target: { value: 'Carolyne.Carter@yahoo.com' },
+      });
+      fireEvent.change(getByLabelText(/first name/i), {
+        target: { value: 'Carolyne' },
+      });
+      fireEvent.change(getByLabelText(/last name/i), {
+        target: { value: 'Carter' },
+      });
+      const submit = getByText('Sign Up');
+      fireEvent.click(submit);
 
-    await mockRegisterResponse();
+      await mockRegisterResponse();
 
-    expect(mockRegisterResponse).toHaveBeenCalledTimes(1);
+      expect(mockRegisterResponse).toHaveBeenCalledTimes(1);
+    });
   });
 });
 
