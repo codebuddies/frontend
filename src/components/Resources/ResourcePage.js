33d4c686bbdb592f9969cc4f35dd8f4e7bb06c9e
Ben Davis <bengineerdavis@gmail.com>
2020-07-26 22:36:15 -0400

    adding MaterialUI.Typography JSX tags to resource page

--- src/components/Resources/ResourcePage.js
+++ src/components/Resources/ResourcePage.js
@@ -3,8 +3,16 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import PersonalMenu from '../PersonalMenu';
-import { Grid, Breadcrumbs, Typography } from '@material-ui/core';
+import { Grid, Breadcrumbs, Typography, Chip } from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  subtitle: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
 
 function ResourcePage({ matchProps }) {
   const [resource, setResource] = useState({});
@@ -22,6 +30,8 @@ function ResourcePage({ matchProps }) {
       });
   }, [matchProps.match.params.id]);
 
+  const classes = useStyles();
+
   return (
     <>
       <Grid container spacing={1}>
@@ -38,7 +48,30 @@ function ResourcePage({ matchProps }) {
             </Link>
             <Typography color="textPrimary">{resource.title}</Typography>
           </Breadcrumbs>
-          <h2>{resource.title}</h2>
+          <Typography variant="h2" gutterBottom>
+            {resource.title}
+          </Typography>
+          <div className={classes.subtitle}>
+            <Typography variant="subtitle1" gutterBottom>
+              <a href={resource.url} target="_blank" rel="noopener noreferrer">
+                {resource.url}
+              </a>
+            </Typography>
+            <Typography variant=" this subtitle1" gutterBottom>
+              <strong>author:</strong> {resource.author}
+            </Typography>
+          </div>
+          <Typography variant="subtitle1" gutterBottom>
+            {resource.media_type}
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            {resource.description}
+          </Typography>
+          {resource.tags &&
+            resource.tags.map(tag => {
+              return <Chip key={tag.name}>{tag.name}</Chip>;
+            })}
+
           <pre>{JSON.stringify(resource, 0, 2)}</pre>
         </Grid>
       </Grid>
