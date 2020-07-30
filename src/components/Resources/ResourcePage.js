e3cb33497e3328512a8dcad0056af5e684b54979
Ben Davis <bengineerdavis@gmail.com>
2020-07-29 20:34:54 -0400

    removing empty div fragment

--- src/components/Resources/ResourcePage.js
+++ src/components/Resources/ResourcePage.js
@@ -33,49 +33,47 @@ function ResourcePage({ matchProps }) {
   const classes = useStyles();
 
   return (
-    <>
-      <Grid container spacing={1}>
-        <Grid item lg={3}>
-          <PersonalMenu />
-        </Grid>
-        <Grid item lg={9}>
-          <Breadcrumbs
-            separator={<NavigateNextIcon fontSize="small" />}
-            aria-label="breadcrumb"
-          >
-            <Link color="inherit" to="/resources">
-              Resources
-            </Link>
-            <Typography color="textPrimary">{resource.title}</Typography>
-          </Breadcrumbs>
-          <Typography variant="h2" gutterBottom>
-            {resource.title}
-          </Typography>
-          <div className={classes.subtitle}>
-            <Typography variant="subtitle1" gutterBottom>
-              <a href={resource.url} target="_blank" rel="noopener noreferrer">
-                {resource.url}
-              </a>
-            </Typography>
-            <Typography variant=" this subtitle1" gutterBottom>
-              <strong>author:</strong> {resource.author}
-            </Typography>
-          </div>
-          <Typography variant="subtitle1" gutterBottom>
-            {resource.media_type}
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            {resource.description}
-          </Typography>
-          {resource.tags &&
-            resource.tags.map(tag => {
-              return <Chip key={tag.name}>{tag.name}</Chip>;
-            })}
-
-          <pre>{JSON.stringify(resource, 0, 2)}</pre>
-        </Grid>
+    <Grid container spacing={1}>
+      <Grid item lg={3}>
+        <PersonalMenu />
       </Grid>
-    </>
+      <Grid item lg={9}>
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          <Link color="inherit" to="/resources">
+            Resources
+          </Link>
+          <Typography color="textPrimary">{resource.title}</Typography>
+        </Breadcrumbs>
+        <Typography variant="h2" gutterBottom>
+          {resource.title}
+        </Typography>
+        <div className={classes.subtitle}>
+          <Typography variant="subtitle1" gutterBottom>
+            <a href={resource.url} target="_blank" rel="noopener noreferrer">
+              {resource.url}
+            </a>
+          </Typography>
+          <Typography variant=" this subtitle1" gutterBottom>
+            <strong>author:</strong> {resource.author}
+          </Typography>
+        </div>
+        <Typography variant="subtitle1" gutterBottom>
+          {resource.media_type}
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          {resource.description}
+        </Typography>
+        {resource.tags &&
+          resource.tags.map(tag => {
+            return <Chip key={tag.name}>{tag.name}</Chip>;
+          })}
+
+        <pre>{JSON.stringify(resource, 0, 2)}</pre>
+      </Grid>
+    </Grid>
   );
 }
 
