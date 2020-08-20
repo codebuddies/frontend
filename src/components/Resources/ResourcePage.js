9402f85a744c56693032c2c1d444ac76278ded8b
bengineerdavis <52514382+bengineerdavis@users.noreply.github.com>
2020-08-19 23:57:51 -0400

    Adding MaterialUI.Typography JSX tags to resource page (#123)
    Create Resource Page.
    Add React Query library.
    Add Axios to utility query.
    
    Co-authored-by: Ben Davis <bengineerdavis@gmail.com>
    Co-authored-by: Linda Peng <linda.peng@alumni.duke.edu>
    Co-authored-by: Angelo Cordon <angelocordon@gmail.com>

--- src/components/Resources/ResourcePage.js
+++ src/components/Resources/ResourcePage.js
@@ -1,48 +1,104 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { getResource } from '../../utils/queries';
 import PersonalMenu from '../PersonalMenu';
-import { Grid, Breadcrumbs, Typography } from '@material-ui/core';
+import { Grid, Breadcrumbs, Typography, Chip, Box } from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  subtitle: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
 
 function ResourcePage({ matchProps }) {
-  const [resource, setResource] = useState({});
+  const resourceId = matchProps.match.params.guid;
+  // TODO: Handle Error cases
+  const { isLoading, data } = useQuery(['resource', resourceId], getResource);
+  const classes = useStyles();
 
-  useEffect(() => {
-    axios
-      .get('/api/v1/resources/' + matchProps.match.params.guid)
-      .then(function(response) {
-        // handle success
-        setResource(response.data);
-      })
-      .catch(function(error) {
-        // handle error
-        console.log(error);
-      });
-  }, [matchProps.match.params.id]);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  const {
+    title,
+    url,
+    author,
+    tags,
+    media_type,
+    description,
+    user,
+    date_published,
+    modified,
+    paid,
+  } = data;
 
   return (
-    <>
-      <Grid container spacing={1}>
-        <Grid item lg={3}>
-          <PersonalMenu />
-        </Grid>
-        <Grid item lg={9}>
-          <Breadcrumbs
-            separator={<NavigateNextIcon fontSize="small" />}
-            aria-label="breadcrumb"
-          >
-            <Link color="inherit" to="/resources">
-              Resources
-            </Link>
-            <Typography color="textPrimary">{resource.title}</Typography>
-          </Breadcrumbs>
-          <h2>{resource.title}</h2>
-          <pre>{JSON.stringify(resource, 0, 2)}</pre>
-        </Grid>
+    <Grid container spacing={1}>
+      <Grid item lg={3}>
+        <PersonalMenu />
       </Grid>
-    </>
+      <Grid item lg={9}>
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          <Link color="inherit" to="/resources">
+            Resources
+          </Link>
+          <Typography color="textPrimary">{title}</Typography>
+        </Breadcrumbs>
+
+        <Typography variant="h2" gutterBottom>
+          {title}
+        </Typography>
+
+        <div className={classes.subtitle}>
+          <Typography variant="subtitle1" gutterBottom>
+            <Link target="_blank" rel="noopener noreferrer" to={url}>
+              {url}
+            </Link>
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            <strong>Author:</strong> {author}
+          </Typography>
+        </div>
+
+        {tags &&
+          tags.map(tag => {
+            return <Chip key={tag.slug} label={tag.name} />;
+          })}
+
+        <div>
+          <Box pt={3}>
+            <Typography variant="subtitle1" gutterBottom>
+              Paid: {paid ? 'yes' : 'no'}
+            </Typography>
+          </Box>
+          <Typography variant="subtitle1" gutterBottom>
+            Media Type: {media_type}
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            Added by <strong>{user.username}</strong> on {date_published}
+          </Typography>
+        </div>
+
+        <Typography variant="subtitle2" gutterBottom>
+          Modified by <strong>"someone"</strong> {modified}
+        </Typography>
+
+        <br></br>
+
+        <Typography variant="body1" gutterBottom>
+          {description}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
 
