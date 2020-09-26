0bf658e122bf959028f53f23a8fbef81595ef492
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2020-09-26 15:13:34 -0700

    Remove Avatar import

--- src/components/Resources/ResourceCard.js
+++ src/components/Resources/ResourceCard.js
@@ -8,7 +8,6 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Collapse from '@material-ui/core/Collapse';
