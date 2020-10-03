c8b73b79817f0021ac5111fee7f9cd8dbfd1ff41
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2020-10-03 15:16:31 -0700

    Some styling for footer

--- src/pages/pageSections/Footer/index.js
+++ src/pages/pageSections/Footer/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <>
+    <footer>
       <hr />
-      &copy; 2020 (in beta) | Contribute:{' '}
+      &copy; 2020 (in alpha) | Contribute:{' '}
       <a href="https://github.com/codebuddies/frontend">Frontend</a> |{' '}
       <a href="https://github.com/codebuddies/backend">Backend</a>
-    </>
+    </footer>
   );
 }
